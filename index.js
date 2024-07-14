4d8bd712eb74ec47ecb03ed71fff8586331c5138
MSeries-kelvin <aramiyorogheneserome@gmail.com>
2024-07-14 12:33:10 +0100

    change main container to 'body' element

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function setFullHeight() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    document.getElementById('app_main_container').style.height = `calc(var(--vh, 1vh) * 100)`;
+    document.getElementById('main-body').style.height = `calc(var(--vh, 1vh) * 100)`;
 }
 
 window.addEventListener('resize', setFullHeight);
