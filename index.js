d7190b0146c8626d04f3a33d52b776120ecde48d
MSeries-kelvin <aramiyorogheneserome@gmail.com>
2024-07-15 08:37:29 +0100

    added functionality for the tab buttons

--- index.js
+++ index.js
@@ -9,3 +9,126 @@ window.addEventListener('orientationchange', setFullHeight);
 window.addEventListener('load', setFullHeight);
 
 setFullHeight();
+
+
+const homeTabBtn = document.getElementById("home_tab_btn");
+const tasksTabBtn = document.getElementById("tasks_tab_btn");
+const profileTabBtn = document.getElementById("profile_tab_btn");
+const main = document.querySelector("main");
+
+homeTabBtn.addEventListener("click", () => {
+    // Disable any active tab
+    var imgs = tasksTabBtn.getElementsByTagName("img");
+    var p = tasksTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/splitscreen_30dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.remove("active");
+    }
+
+    var imgs = profileTabBtn.getElementsByTagName("img");
+    var p = profileTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/person_30dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.remove("active");
+    }
+
+    // Enable this tab
+    var imgs = homeTabBtn.getElementsByTagName("img");
+    var p = homeTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/home_30dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.add("active");
+    }
+
+    main.style.backgroundImage = "url(icons/home_30dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg)";
+});
+
+tasksTabBtn.addEventListener("click", () => {
+    // Disable any active tab
+    var imgs = homeTabBtn.getElementsByTagName("img");
+    var p = homeTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/home_30dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.remove("active");
+    }
+
+    var imgs = profileTabBtn.getElementsByTagName("img");
+    var p = profileTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/person_30dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.remove("active");
+    }
+
+    // Enable this tab
+    var imgs = tasksTabBtn.getElementsByTagName("img");
+    var p = tasksTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/splitscreen_30dp_5F6368_FILL1_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.add("active");
+    }
+
+    main.style.backgroundImage = "url(icons/splitscreen_30dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg)";
+});
+
+profileTabBtn.addEventListener("click", () => {
+    // Disable any active tab
+    var imgs = homeTabBtn.getElementsByTagName("img");
+    var p = homeTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/home_30dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.remove("active");
+    }
+
+    var imgs = tasksTabBtn.getElementsByTagName("img");
+    var p = tasksTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/splitscreen_30dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.remove("active");
+    }
+
+    // Enable this tab
+    var imgs = profileTabBtn.getElementsByTagName("img");
+    var p = profileTabBtn.getElementsByTagName("p");
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].src = "icons/person_30dp_5F6368_FILL1_wght400_GRAD0_opsz24.svg";
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].classList.add("active");
+    }
+
+    main.style.backgroundImage = "url(icons/person_30dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg)";
+});
